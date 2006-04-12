4e54756014c57c5f8d474932553c68f140b80f5e
goliath <goliath@423c3ab3-1af6-0310-84a2-dae6aeac5b19>
2006-04-12 08:37:55 +0000

    * Changed license of rrd-classes to LGPL
    git-svn-id: http://svn.berlios.de/svnroot/repos/serverstats/trunk@125 423c3ab3-1af6-0310-84a2-dae6aeac5b19

--- includes/rrd.class.php
+++ includes/rrd.class.php
@@ -4,23 +4,23 @@
  *
  * Author: David Danier, david.danier@team23.de
  * Project: Serverstats, http://serverstats.berlios.de/
- * License: GPL v2 or later (http://www.gnu.org/licenses/gpl.html)
+ * License: LGPL v2.1 or later (http://www.gnu.org/licenses/lgpl.html)
  *
  * Copyright (C) 2005 David Danier
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 class rrd
