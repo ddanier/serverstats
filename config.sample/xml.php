831dfd0983aae2166d4e6698909e1c2e88c2af9f
scramatte <scramatte@423c3ab3-1af6-0310-84a2-dae6aeac5b19>
2006-07-03 20:19:29 +0000

    Add xmlconfig support
    git-svn-id: http://svn.berlios.de/svnroot/repos/serverstats/branches/xmlconfig2@155 423c3ab3-1af6-0310-84a2-dae6aeac5b19

---
+++ config.sample/xml.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * $Id: simple.php 123 2006-04-11 12:35:34Z goliath $
+ *
+ * Author: David Danier, david.danier@team23.de
+ * Project: Serverstats, http://serverstats.berlios.de/
+ * License: GPL v2 or later (http://www.gnu.org/licenses/gpl.html)
+ *
+ * Copyright (C) 2005 David Danier
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+//$config['used'] = true; // is the simple config used
+
+$dom = new domDocument();
+$dom->load(CONFIGPATH . 'xml/sources.xml');
+$dom->Xinclude();
+
+$config['modules'] = xmlconfig::read($dom);
+$config['used'] = true;
+
+//print_r ($config);
+?>
