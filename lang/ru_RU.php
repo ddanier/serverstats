2df195ce2b171e7e7afbac527ad494c454c60082
goliath <goliath@423c3ab3-1af6-0310-84a2-dae6aeac5b19>
2010-05-28 15:19:29 +0000

    Added russian translation (thanks to Ivan Kliouchenkov)
    git-svn-id: http://svn.berlios.de/svnroot/repos/serverstats/trunk@180 423c3ab3-1af6-0310-84a2-dae6aeac5b19

---
+++ lang/ru_RU.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * $Id$
+ *
+ * Author: David Danier, david.danier@team23OC.de
+ * Russian translation : Ivan Klyushenkov, kluisure@gmail.com
+ *
+ * Project: Serverstats, http://serverstats.berlios.de/
+ * License: GPL v2 or later (http://www.gnu.org/licenses/gpl.html)
+ *
+ * Copyright (C) 2005 David Danier
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+$lang = array();
+$lang['Statistics'] = 'Статистика';
+$lang['Summary'] = 'Сводная';
+$lang['Tree'] = 'Дерево';
+$lang['Graphs'] = 'Графики';
+
+$lang['Information'] = 'Информация';
+$lang['Warning'] = 'Предупреждение';
+$lang['Error'] = 'Ошибка';
+$lang['Unknown error'] = 'Неизвестная ошибка';
+
+?>
